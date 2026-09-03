ebdf14ccc290e22c3ce5036155707c89315aaf4a
menochef777 <menochef777@users.noreply.github.com>
2026-09-03 18:31:43 -0300

    feat: add Matteo custom 3D avatar with magnet interactive effect

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -12,7 +12,7 @@ export interface Project {
 }
 
 export const HERO_ASSETS = {
-  portrait: "https://shrug-person-78902957.figma.site/_components/v2/d24c01ad3a56fc65e942a1f501eb73db42d7cf9a/Rectangle_40443.81459862.png",
+  portrait: "/matteo-portrait.png",
 };
 
 export const MARQUEE_ROW_1 = [
